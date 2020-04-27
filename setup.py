1c1b4245e7061da65a26b125028276870e2197f5
Ingo Heimbach <i.heimbach@fz-juelich.de>
2020-04-27 11:14:23 +0200

    Remove `requirements.txt` to avoid confusion
    By convention, `requirements.txt` is used to describe concrete Python
    environments, but in this project the file was used to describe package
    dependencies. Thus, remove `requirements.txt` and declare dependencies
    in the `setup.py` script directly.

--- setup.py
+++ setup.py
@@ -8,17 +8,6 @@ def get_version_from_pyfile(version_file="nojava_ipmi_kvm/_version.py"):
     return file_globals["__version__"]
 
 
-def get_install_requires_from_requirements(requirements_filename="requirements.txt"):
-    try:
-        with open(requirements_filename, "r", encoding="utf-8") as requirements_file:
-            requirements = requirements_file.readlines()
-    except OSError:
-        import logging
-
-        logging.warning("Could not read the requirements file.")
-    return requirements
-
-
 def get_long_description_from_readme(readme_filename="README.md"):
     long_description = None
     if os.path.isfile(readme_filename):
@@ -29,16 +18,14 @@ def get_long_description_from_readme(readme_filename="README.md"):
 
 version = get_version_from_pyfile()
 long_description = get_long_description_from_readme()
-install_requires = get_install_requires_from_requirements()
-install_requires_gui = get_install_requires_from_requirements("requirements-gui.txt")
 
 setup(
     name="nojava-ipmi-kvm",
     version=version,
     packages=find_packages(),
     python_requires="~=3.5",
-    install_requires=install_requires,
-    extras_require={"GUI": install_requires_gui},
+    install_requires=["requests", "yacl"],
+    extras_require={"GUI": ["PyQt5>=5.12", "PyQtWebEngine>=5.12"]},
     entry_points={"console_scripts": ["nojava-ipmi-kvm = nojava_ipmi_kvm.cli:main"]},
     author="Ingo Heimbach",
     author_email="i.heimbach@fz-juelich.de",
